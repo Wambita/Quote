e1c9b7341323c1cc15cef8d6584de110d733afea
Sam Kasyoki <samkasyoki@gmail.com>
2018-10-19 15:45:39 +0300

    import the forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './components/quote/quote.component';
+import { QuoteFormComponent } from './components/quote-form/quote-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, QuoteComponent, QuoteFormComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
