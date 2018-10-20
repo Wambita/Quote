7cf00ebeb99ff40b6dce07a197a413538f0c6640
Sam Kasyoki <samkasyoki@gmail.com>
2018-10-20 22:20:22 +0300

    successfully create a quote

--- src/app/components/quote-form/quote-form.component.ts
+++ src/app/components/quote-form/quote-form.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  ViewChild
+} from '@angular/core';
 import { Quote } from '../../models/quote/quote';
+import { Quotes } from '../../models/quotes/quotes';
 
 @Component({
   selector: 'app-quote-form',
@@ -7,8 +14,22 @@ import { Quote } from '../../models/quote/quote';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+  @Output()
+  formQuote = new EventEmitter();
+
+  @ViewChild('quoteForm')
+  form: any;
+
   newQuote = new Quote(0, '', '', '');
   constructor() {}
 
   ngOnInit() {}
+
+  addNewQuote({ value }) {
+    this.newQuote.id = Quote.length + 1;
+    this.newQuote.createdOn = new Date();
+    console.log(this.newQuote);
+    Quotes.unshift(value);
+    this.form.reset();
+  }
 }
