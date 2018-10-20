91a8bddbe5a5ffc35b4f054cbe40549a1a43e711
Sam Kasyoki <samkasyoki@gmail.com>
2018-10-20 22:19:38 +0300

    create a model for storing the quotes array

---
+++ src/app/models/quotes/quotes.ts
@@ -0,0 +1,6 @@
+import { Quote } from '../quote/quote';
+
+export const Quotes: Quote[] = [
+  new Quote(0, 'I am a quote', 'sam', 'carlos'),
+  new Quote(1, 'I am a quote too', 'carlos', 'sam')
+];
