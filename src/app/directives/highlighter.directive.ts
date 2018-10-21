a791646e05f5cbc7fe2f12e97ae4e3c713d5454e
Sam Kasyoki <samkasyoki@gmail.com>
2018-10-21 23:46:21 +0300

    create a directive that highlights the most voted quote

--- src/app/directives/highlighter.directive.ts
+++ src/app/directives/highlighter.directive.ts
@@ -1,10 +1,39 @@
-import { Directive } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  Input,
+  Renderer2,
+  HostListener,
+  OnChanges,
+  OnInit,
+  SimpleChanges
+} from '@angular/core';
 
 @Directive({
   selector: '[appHighlighter]'
 })
-export class HighlighterDirective {
+export class HighlighterDirective implements OnChanges {
+  @Input()
+  appHighlighter;
 
-  constructor() { }
+  constructor(private renderer: Renderer2, private elem: ElementRef) {}
 
+  @HostListener('change')
+  ngOnChanges() {
+    if (this.appHighlighter) {
+      this.addClass('custom-badge', this.elem.nativeElement);
+    } else {
+      this.removeClass('custom-badge', this.elem.nativeElement);
+    }
+  }
+
+  addClass(className: string, element: any) {
+    this.renderer.addClass(element, className);
+    element.style.display = 'block';
+  }
+
+  removeClass(className: string, element: any) {
+    this.renderer.removeClass(element, className);
+    element.style.display = 'none';
+  }
 }
