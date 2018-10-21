60594735b9243b76405047863837c403e021dcf4
Sam Kasyoki <samkasyoki@gmail.com>
2018-10-21 23:44:24 +0300

    create a separate model for the quotes array

--- src/app/models/quotes/quotes.ts
+++ src/app/models/quotes/quotes.ts
@@ -1,6 +1,40 @@
 import { Quote } from '../quote/quote';
 
 export const Quotes: Quote[] = [
-  new Quote(0, 'I am a quote', 'sam', 'carlos'),
-  new Quote(1, 'I am a quote too', 'carlos', 'sam')
+  new Quote(
+    1,
+    'Very little is needed to make a happy life; it is all within yourself, in your way of thinking',
+    'Marcus Aurelius',
+    'Sam Kasyoki',
+    12,
+    2,
+    new Date('October 01, 2018 11:24:00')
+  ),
+  new Quote(
+    2,
+    'Dost thou love life? Then do not squander time, for that is the stuff life is made of',
+    'Benjamin Franklin',
+    'Sally Mbinya',
+    10,
+    0,
+    new Date('September 19, 2018 03:24:00')
+  ),
+  new Quote(
+    3,
+    'I’m the one that’s got to die when it’s time for me to die, so let me live my life the way I want to.',
+    ' Jimi Hendrix',
+    'Kevin Gates',
+    8,
+    5,
+    new Date('October 18, 2018 01:20:00')
+  ),
+  new Quote(
+    4,
+    'Anyone who lives within their means suffers from a lack of imagination.',
+    ' Oscar Wilde',
+    'Jimmy Gait',
+    10,
+    5,
+    new Date('October 20, 2018 00:19:00')
+  )
 ];
